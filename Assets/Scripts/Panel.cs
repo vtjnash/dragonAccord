9f0170ff8e89de3b04f538468dbd0683ec05c832
Sniejadlik <sniejadlik@gmail.com>
2016-10-09 16:04:47 -0400

    Final Push
    Final Project Push to Git Hub. Dragon Accord 1.0 Is a gO

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Panel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnSelect()
+    {
+        Destroy(gameObject);
+        //gameObject.SetActive(false);
+    }
+}
